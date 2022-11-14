12e07fcf6916b4e03c6c98225d03a5a554bbc773
GJ Tiquia <gershom.tiquia@gmail.com>
2022-11-14 21:19:07 +0800

    dynamic clipping material

--- Assets/Scripts/GameScene/CameraManager.cs
+++ Assets/Scripts/GameScene/CameraManager.cs
@@ -12,11 +12,11 @@ namespace GameScene
 
         private void Start()
         {
-            Vector3 lowerLeftPoint = _mainCamera.WorldToScreenPoint(new Vector3(-5, -0.5f, -5));
-            Vector3 lowerRightPoint = _mainCamera.WorldToScreenPoint(new Vector3(5, -0.5f, -5));
+            // Vector3 lowerLeftPoint = _mainCamera.WorldToScreenPoint(new Vector3(-5, -0.5f, -5));
+            // Vector3 lowerRightPoint = _mainCamera.WorldToScreenPoint(new Vector3(5, -0.5f, -5));
 
-            DebugLog(lowerLeftPoint);
-            DebugLog(lowerRightPoint);
+            // DebugLog(lowerLeftPoint);
+            // DebugLog(lowerRightPoint);
         }
 
         private void LateUpdate()
@@ -29,8 +29,7 @@ namespace GameScene
                 _mainCamera.transform.position = cameraPosition;
 
                 // Calculate camera orthgraphic size
-                // TODO: temporarily arbitrarily set to the game size
-                _mainCamera.orthographicSize = _gameManager.GameSize;
+                _mainCamera.orthographicSize = _gameManager.GameSize; // temporarily set to the game size
 
                 _isCameraSet = true;
             }
