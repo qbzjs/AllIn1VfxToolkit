5de7c27cf56be2dc2e458fa72a5869643cb9027e
GJ Tiquia <gershom.tiquia@gmail.com>
2022-11-25 23:12:55 +0800

    perspective camera can do dynamic 2D, 3D, 4D

--- Assets/Scripts/GameScene/CameraManager.cs
+++ Assets/Scripts/GameScene/CameraManager.cs
@@ -7,7 +7,8 @@ namespace GameScene
         [SerializeField] Camera _mainCamera;
         [SerializeField] GameManager _gameManager;
         [SerializeField] MeshRenderer _platformMeshRenderer;
-        [SerializeField] float _orthographicSizeFactor = 1;
+        [SerializeField] float _cameraHeight = 20f; // The intended fixed y position
+        [SerializeField] float _orthographicSizeFactor = 1; // Converts game size to orthographic size
 
         bool _isCameraSet = false;
 
@@ -15,16 +16,24 @@ namespace GameScene
         {
             if (!_isCameraSet)
             {
-                // Calculate camera position
-                Vector3 cameraPosition = _platformMeshRenderer.bounds.center;
-                cameraPosition.y = _mainCamera.transform.position.y;
-                _mainCamera.transform.position = cameraPosition;
-
-                // Calculate camera orthgraphic size
-                _mainCamera.orthographicSize = _gameManager.GameSize * _orthographicSizeFactor;
+                _mainCamera.transform.position = CalculateFlatCameraWorldPosition(_platformMeshRenderer, _cameraHeight);
+                _mainCamera.orthographicSize = CalculateFlatCameraOrthographicize(_gameManager.GameSize, _orthographicSizeFactor);
 
                 _isCameraSet = true;
             }
+        }
+
+        public static Vector3 CalculateFlatCameraWorldPosition(MeshRenderer platformMeshRenderer, float cameraHeight)
+        {
+            Vector3 cameraPosition = platformMeshRenderer.bounds.center;
+            cameraPosition.y = cameraHeight;
+            // cameraPosition.y = _mainCamera.transform.position.y;
+            return cameraPosition;
+        }
+
+        public static float CalculateFlatCameraOrthographicize(int gameSize, float orthographicSizeFactor)
+        {
+            return gameSize * orthographicSizeFactor;
         }
     }
 }
--- Assets/Scripts/GameScene/PerspectiveCameraZScaling.cs
+++ Assets/Scripts/GameScene/PerspectiveCameraZScaling.cs
@@ -6,15 +6,58 @@ namespace GameScene
 {
     public class PerspectiveCameraZScaling : MonoBehaviour
     {
+        public static int SizeReference => SIZE_REFERENCE;
+        public const int SIZE_REFERENCE = 10;
+
         [SerializeField] GameManager _gameManager;
+
+        [Header("Perspective Camera Settings")]
         [SerializeField] float _zReference = -32f;
 
-        const int SIZE_REFERENCE = 10;
+        [Header("Orthographic Camera Settings")]
+        [SerializeField] Transform _parentTransform;
+        [SerializeField] MeshRenderer _platformMeshRenderer;
+        [SerializeField] float _cameraHeight = 20f; // The intended fixed y position
+        [SerializeField] float _orthographicSizeFactor = 0.5f; // Converts game size to orthographic size
 
-        void Start()
+        Camera _thisCamera;
+        bool _setCamera = false;
+        bool _haveSkippedFirstFrame = false;
+
+        void LateUpdate()
         {
-            float z = _zReference * _gameManager.GameSize / SIZE_REFERENCE;
-            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, z);
+            if (_setCamera) return;
+            StartCoroutine(SetCamera());
+            _setCamera = true;
+        }
+
+        private IEnumerator SetCamera()
+        {
+            yield return null; // Need to skip a frame before can set the absolute world position directly, especially in DimensionTwo
+            _thisCamera = GetComponent<Camera>();
+
+            if (_gameManager.GameDimension == Snake4D.Dimension.DimensionTwo)
+            {
+                _thisCamera.orthographic = true;
+                transform.localEulerAngles = new Vector3(45, 0, 0);
+
+                transform.position = CameraManager.CalculateFlatCameraWorldPosition(_platformMeshRenderer, _cameraHeight);
+                _thisCamera.orthographicSize = CameraManager.CalculateFlatCameraOrthographicize(_gameManager.GameSize, _orthographicSizeFactor);
+            }
+
+            else if (_gameManager.GameDimension == Snake4D.Dimension.DimensionThree || _gameManager.GameDimension == Snake4D.Dimension.DimensionFour)
+            {
+                _thisCamera.orthographic = false;
+                transform.localEulerAngles = new Vector3(0, 0, 0);
+
+                float z = CalculateLocalZScaling(_zReference, _gameManager.GameSize);
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, z);
+            }
+        }
+
+        public static float CalculateLocalZScaling(float zReference, int gameSize)
+        {
+            return zReference * gameSize / SizeReference;
         }
     }
 }
