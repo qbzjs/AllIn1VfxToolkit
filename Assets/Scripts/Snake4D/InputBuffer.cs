cb1e2037232fe8febcc0d9929ebec1f547246513
GJ Tiquia <gershom.tiquia@gmail.com>
2022-11-30 09:59:39 +0800

    input buffer checking if input is repeated

--- Assets/Scripts/Snake4D/InputBuffer.cs
+++ Assets/Scripts/Snake4D/InputBuffer.cs
@@ -12,11 +12,26 @@ namespace Snake4D
             _bufferCapacity = bufferCapacity;
         }
 
-        public void AddInput(UserInputType input, Dimension dimension)
+        /// <summary>
+        /// Returns true if input is valid.
+        /// Input is valid for the current dimension and if it is not repeated.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="dimension"></param>
+        public bool AddInput(UserInputType input, Dimension dimension)
         {
-            if (dimension == Dimension.DimensionOne && (int)input > (int)UserInputType.X_Negative) return;
-            if (dimension == Dimension.DimensionTwo && (int)input > (int)UserInputType.Y_Negative) return;
-            if (dimension == Dimension.DimensionThree && (int)input > (int)UserInputType.Z_Negative) return;
+            if (dimension == Dimension.DimensionOne && (int)input > (int)UserInputType.X_Negative)
+                return false;
+
+            if (dimension == Dimension.DimensionTwo && (int)input > (int)UserInputType.Y_Negative)
+                return false;
+
+            if (dimension == Dimension.DimensionThree && (int)input > (int)UserInputType.Z_Negative)
+                return false;
+
+            // Prevent the same input from being added multiple times
+            if (_inputQueue.Count > 0 && _inputQueue.Peek() == input)
+                return false;
 
             if (_inputQueue.Count == _bufferCapacity)
             {
@@ -24,6 +39,7 @@ namespace Snake4D
             }
 
             _inputQueue.Enqueue(input);
+            return true;
         }
 
         public UserInputType GetInput()
